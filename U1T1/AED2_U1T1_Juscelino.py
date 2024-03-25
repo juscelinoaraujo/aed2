7c71bca3ad8fe9b3515dc6f5b21ae0299ef986f1
Juscelino Pereira de Araújo <juscelino.caico@gmail.com>
2024-03-24 21:47:44 -0300

    Final file

--- U1T1/AED2_U1T1_Juscelino.py
+++ U1T1/AED2_U1T1_Juscelino.py
@@ -6,80 +6,52 @@ response = requests.get("https://raw.githubusercontent.com/juscelinoaraujo/aed2/
 print(response.text)
 biblia_string = response.text
 
-# Em seguida, são removidos caracteres especiais, incluindo números e sinais de pontuação."""
-
 caracteres_removiveis = "0123456789,.;?[]{}'!@#$%&*()_^:"
 for c in caracteres_removiveis:
   biblia_string = biblia_string.replace(c, "")
 
-# O passo seguinte é a conversão de todas as letras para minúsculas."""
 
 biblia_string = biblia_string.lower()
-
-# Além disso, letras (no caso do português somente as vogais) com acentuação tem seus acentos removidos."""
-
-#a_com_acento = "áàãâ"
-#e_com_acento = "éê"
-#o_com_acento = "óõô"
-#for l in a_com_acento:
-#  biblia_string = biblia_string.replace(l, "a")
-#for l in e_com_acento:
-#  biblia_string = biblia_string.replace(l, "e")
-#for l in o_com_acento:
-#  biblia_string = biblia_string.replace(l, "o")
-#biblia_string = biblia_string.replace("í", "i")
-#biblia_string = biblia_string.replace("ú", "u")
-#biblia_string = biblia_string.replace("ç", "c")
-
-# Removidos os caracteres especiais e trocadas as letras com acentos, a string é dividida em palavras. A separação acontece por meio de espaços. Por isso, as quebras de linhas e os hífens são inicialmente convertidas em espaços para que a separação ocorra normalmente. Com isso, é criado o arquivo `biblia_lista`, que consiste em uma lista com as palavras obtidas da separação realizada."""
 
 biblia_string = biblia_string.replace("\n", " ")
 biblia_string = biblia_string.replace("\r", " ")
 biblia_string = biblia_string.replace("-", " ")
 biblia_lista = biblia_string.split(" ")
 
-# Algumas das palavras não são de interesse, como artigos, preposições e algumas conjunções. Elas são retiradas da lista."""
+biblia_lista = list(set(biblia_lista))
 
 palavras_removiveis = [" ", "o", "os", "a", "as", "um", "uma", "de", "do", "dos", "da", "das", "dum", "duma", "em", "no", "nos", "na", "nas", "para", "que", "mas", "e", "se"]
 for p in palavras_removiveis:
   if p in biblia_lista:
     biblia_lista.remove(p)
 
-# Como seria de se esperar um texto tão grande, muitas palavras são repetidas. O código executado a seguir remove as palavras repetidas. Isso é feito gerando uma estrutura de conjunto com os elementos da lista e depois gerando uma nova lista a partir desse conjunto. Convém lembrar que isso advém do fato de que em estruturas de tipo conjunto não há multiplicidade de elementos."""
-
 biblia_lista = list(set(biblia_lista))
-
-# A lista de palavras é então ordenada, sendo armazenada na variável `biblia_lista_ordenada`:"""
-
 biblia_lista_ordenada = sorted(biblia_lista)
 
-# Já temos uma lista de palavras, o que funcionará como estrutura linear de busca para efeitos de comparção. Quanto à estrutura em árvore, na seção a seguir são descritas as classes previamente criadas e que seriam utilizadas aqui.
-
-### Árvores AVL
 
 class Node:
 
     def __init__(self, value):
-        
+
         self.value = value
         self.left_child = None
         self.right_child = None
 
+
 class BST:
 
     def __init__(self):
-        
         self.root = None
 
     def add(self, value):
-        
+
         if self.root is None:
             self.root = Node(value)
         else:
             self._add_recursive(self.root, value)
 
     def _add_recursive(self, current_node, value):
-        
+
         if value <= current_node.value:
             if current_node.left_child is None:
                 current_node.left_child = Node(value)
@@ -92,7 +64,7 @@ class BST:
                 self._add_recursive(current_node.right_child, value)
 
     def _contains(self, current_node, value):
-       
+
         if current_node is None:
             return False
         if current_node.value == value:
@@ -102,18 +74,18 @@ class BST:
         return self._contains(current_node.right_child, value)
 
     def contains(self, value):
+
         return self._contains(self.root, value)
-        
+
+
 class AVLNode(Node):
 
     def __init__(self, value):
-        
         super().__init__(value)
         self.height = 1
         self.imbalance = 0
 
     def calculate_height_and_imbalance(self):
-        
         # Calculate the height of the left child subtree
         left_height = 0
         if self.left_child is not None:
@@ -133,11 +105,9 @@ class AVLNode(Node):
 class AVLTree(BST):
 
     def __init__(self):
-        
         super().__init__()
 
     def add(self, value):
-        
         self.root = self._add_recursive(self.root, value)  # Note that self.root is updated here
 
 
@@ -172,13 +142,11 @@ class AVLTree(BST):
         return current_node
 
     def get_height(self):
-        
         if self.root is None:
             return 0
         return self.root.height
 
     def _rotate_left(self, node):
-        
 
         # Store the pivot (the root of the right subtree of 'node')
         pivot = node.right_child
@@ -200,7 +168,6 @@ class AVLTree(BST):
 
 
     def _rotate_right(self, node):
-        
 
         # Store the pivot (the root of the left subtree of 'node')
         pivot = node.left_child
@@ -221,7 +188,6 @@ class AVLTree(BST):
         return pivot
 
     def _balance(self, node):
-      
 
       # Case 1: Left subtree is higher than right subtree
       if node.imbalance == 2:
@@ -244,8 +210,16 @@ class AVLTree(BST):
               node.right_child = self._rotate_right(pivot)
               return self._rotate_left(node)
 
-
-### Função de Autocompletamento de Palavras
+def autocomplete_lista_ordenada(lista, palavra):
+  saida = []
+  for p in lista:
+    if p[0:len(palavra)] < palavra:
+      continue
+    elif p[0:len(palavra)] == palavra:
+      saida.append(p)
+    else:
+      break;
+  return saida
 
 def autocomplete_no(no, prefixo):
   saida = []
@@ -259,8 +233,8 @@ def autocomplete_no(no, prefixo):
       saida += autocomplete_no(no.right_child, prefixo)
   return saida
 
-def autocomplete_arvore_AVL(arvore, palavra):
-  return sorted(autocomplete_no(arvore.root, palavra))
+def autocomplete_arvore_AVL(arvore, prefixo):
+  return sorted(autocomplete_no(arvore.root, prefixo))
 
 biblia_arvore_AVL = AVLTree();
 for p in biblia_lista_ordenada:
@@ -274,4 +248,3 @@ prefixo = st.text_input("Digite o prefixo que deseja pesquisar: ")
 
 if len(prefixo) != 0:
   st.write(autocomplete_arvore_AVL(biblia_arvore_AVL, prefixo))
-	
